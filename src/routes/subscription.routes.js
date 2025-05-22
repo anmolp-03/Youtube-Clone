c15e63c0c2b83703e257535c6063d58dd7736199
anmolp-03 <panjwanianmol03@gmail.com>
2025-05-22 17:10:59 +0530

    Subscription Controllers structure decided

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import {
+    getSubscribedChannels,
+    getUserChannelSubscribers,
+    toggleSubscription,
+} from "../controllers/subscription.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
